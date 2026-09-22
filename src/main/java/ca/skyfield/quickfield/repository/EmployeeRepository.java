1901a0272a8da015392f8862ca87296300d3b1c1
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-09-22 01:24:21 -0400

    Update with query to load all employees with their tasks

--- src/main/java/ca/skyfield/quickfield/repository/EmployeeRepository.java
+++ src/main/java/ca/skyfield/quickfield/repository/EmployeeRepository.java
@@ -2,6 +2,12 @@ package ca.skyfield.quickfield.repository;
 
 import ca.skyfield.quickfield.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT DISTINCT e FROM Employee e LEFT JOIN FETCH e.tasks")
+    List<Employee> findAllWithTasks();
 }
