dcd58fa34dacc9686960b84c1f00b56e41300828
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-09-23 00:44:02 -0400

    Add employee request dto validation

--- src/main/java/ca/skyfield/quickfield/dto/EmployeeRequest.java
+++ src/main/java/ca/skyfield/quickfield/dto/EmployeeRequest.java
@@ -1,14 +1,23 @@
 package ca.skyfield.quickfield.dto;
 
 import ca.skyfield.quickfield.dto.TaskRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record EmployeeRequest(
         Long id,
+
+        @NotBlank(message = "First name is required")
         String firstName,
+
+        @NotBlank(message = "Last name is required")
         String lastName,
+
+        @NotBlank(message = "Phone number is required")
         String phone,
-        List<Long> taskIds
+
+        List<@NotNull Long> taskIds
 ) {
 }
