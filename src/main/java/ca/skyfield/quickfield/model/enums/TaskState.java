84689b2f0e63f6c90011e539b36b78995b6cc793
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-09-24 19:20:43 -0400

    Add canTransitionTo for TaskState transition rules

--- src/main/java/ca/skyfield/quickfield/model/enums/TaskState.java
+++ src/main/java/ca/skyfield/quickfield/model/enums/TaskState.java
@@ -1,8 +1,18 @@
 package ca.skyfield.quickfield.model.enums;
 
 public enum TaskState {
-    CREATED,
+    TODO,
     IN_PROGRESS,
     CANCELLED,
-    COMPLETED
+    COMPLETED;
+
+    public boolean canTransitionTo(TaskState targetState) {
+        return switch (this) {
+            case TODO -> targetState == IN_PROGRESS || targetState == CANCELLED;
+            case IN_PROGRESS -> targetState == COMPLETED || targetState == CANCELLED;
+            case COMPLETED,CANCELLED -> false;
+        };
+    }
+
 }
+
