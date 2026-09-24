912426b85a51649ed50bc78675b86648cd08dcca
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-09-24 19:22:03 -0400

    Add spring main application

---
+++ src/main/java/ca/skyfield/quickfield/QuickfieldApplication.java
@@ -0,0 +1,13 @@
+package ca.skyfield.quickfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfieldApplication.class, args);
+	}
+
+}
